ebc388e0fde7c452670f154c49c19990c0499eb2
Ekhidna <ekhidna@disroot.org>
2026-03-05 21:47:40 +0700

    notes auto move

--- js/main.js
+++ js/main.js
@@ -9,6 +9,35 @@ new Vue({
         maxCol2: 5
     },
     methods: {
+        getCompletionPercentage(card) {
+            if (!card.items.length) return 0;
+            const completed = card.items.filter(item => item.completed).length;
+            return (completed / card.items.length) * 100;
+        },
+        moveCard(card, fromCol, toCol) {
+            const index = this[fromCol].findIndex(c => c.id === card.id);
+            if (index !== -1) {
+                this[fromCol].splice(index, 1);
+                this[toCol].push(card);
+            }
+        },
+        onItemToggle(card) {
+            const percentage = this.getCompletionPercentage(card);
+
+            let currentCol = 'col1';
+            if (this.col2.includes(card)) currentCol = 'col2';
+            if (this.col3.includes(card)) currentCol = 'col3';
+
+            if (percentage === 100) {
+                if (currentCol === 'col1' || currentCol === 'col2') {
+                    this.moveCard(card, currentCol, 'col3');
+                }
+            } else if (percentage > 50 && currentCol === 'col1') {
+                if (this.col2.length < this.maxCol2) {
+                    this.moveCard(card, 'col1', 'col2');
+                }
+            }
+        },
         addCard() {
             if (this.col1.length >= this.maxCol1) return;
             const newCard = {
