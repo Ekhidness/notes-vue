928a8eb55034fa74dcc0c161f02d3713a00dd57a
Ekhidna <ekhidna@disroot.org>
2026-03-05 18:38:46 +0700

    adding cards release

--- js/main.js
+++ js/main.js
@@ -4,6 +4,23 @@ new Vue({
         col1: [],
         col2: [],
         col3: [],
-        nextId: 1
+        nextId: 1,
+        maxCol1: 3,
+        maxCol2: 5
+    },
+    methods: {
+        addCard() {
+            if (this.col1.length >= this.maxCol1) return;
+            const newCard = {
+                id: this.nextId++,
+                title: 'Новая карточка',
+                items: [
+                    { id: this.nextId++, text: '', completed: false },
+                    { id: this.nextId++, text: '', completed: false },
+                    { id: this.nextId++, text: '', completed: false }
+                ]
+            };
+            this.col1.push(newCard);
+        }
     }
 });
